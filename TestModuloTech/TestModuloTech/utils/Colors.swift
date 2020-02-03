a878f221e6eaf262a283adad43af7a4af71d074c
ndennu <dennu.nicolas@gmail.com>
2020-02-03 15:05:14 +0100

    update colors.swift

---
+++ TestModuloTech/TestModuloTech/utils/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  TestModuloTech
+//
+//  Created by Nicolas DENNU on 28/01/2020.
+//  Copyright © 2020 ndennu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static let darkBlue = UIColor(red:0.15, green:0.28, blue:0.46, alpha:1.0)
+    static let lightBlue = UIColor(red:0.45, green:0.69, blue:0.74, alpha:1.0)
+    static let yellow = UIColor(red:0.98, green:0.84, blue:0.34, alpha:1.0)
+    static let lightGray = UIColor(red:0.55, green:0.60, blue:0.67, alpha:1.0)
+    static let lightBlack = UIColor(red:0.29, green:0.29, blue:0.29, alpha:1.0)
+    static let white = UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0)
+    static let darkWhite = UIColor(red:0.96, green:0.96, blue:0.96, alpha:1.0)
+    static let redColor = UIColor(red:0.79, green:0.23, blue:0.20, alpha:1.0)
+    static let grayColor = UIColor(red:0.80, green:0.82, blue:0.85, alpha:1.0)
+}
